9bba8a2acdeabfe13abd33cbb29f27d8302dc26a
LENOVO <chrysolite77ci@gmail.com>
2024-11-24 19:28:31 +0000

    committing to the main Branch

---
+++ Casaconnect-main/db/connect.php
@@ -0,0 +1,17 @@
+<?php
+// Database connection file
+$servername = "localhost";
+$username = "root";       
+$password = "";      
+$database = "casaconnect"; 
+
+// Create a new database connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}else{
+    echo "Connection To Database Successful.";
+}
+?>
